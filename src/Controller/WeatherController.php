803bbb6973105b323726031c597ebc94992a4e4c
vincentDubresson <vincent-dubresson_student2022@wilder.school>
2022-05-12 15:54:37 +0200

    recupération de la météo de la ville rentrée dans l'input

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -11,17 +11,37 @@ class WeatherController extends AbstractController
      */
     public function index(): string
     {
-        $weatherManager = new WeatherManager();
-        $location = $weatherManager->getLocationByName('Lyon');
+        if (($_SERVER['REQUEST_METHOD']) === 'POST') {
+            $errors = [];
+            $city = trim($_POST['city']);
+            if (empty($city)) {
+                $errors = 'error';
+            }
+            if (strlen($city) > 80) {
+                $errors = 'error';
+            }
+            if (empty($errors)) {
+                $weatherManager = new WeatherManager();
+                $location = $weatherManager->getLocationByName($city);
+                $insee = $location['cities'][0]['insee'];
+                $weathers = $weatherManager->getWeatherByInsee($insee);
 
+                return $this->twig->render('Home/index.html.twig', [
+                    'location' => $location,
+                    'weathers' => $weathers
+                ]);
+            }
+        }
+
+        $weatherManager = new WeatherManager();
+        $location = $weatherManager->getLocationByName('lyon');
         $insee = $location['cities'][0]['insee'];
 
         $weathers = $weatherManager->getWeatherByInsee($insee);
 
-        return $this->twig->render('Item/index.html.twig', [
+        return $this->twig->render('Home/index.html.twig', [
             'location' => $location,
             'weathers' => $weathers
         ]);
     }
 }
-
