f8a464e4a0fee1c02cfa3ecc280f27e8aea9c3b7
Anthony Gouton <anthony.gouton.wcs@gmail.com>
2022-05-12 23:29:11 +0200

    add locate script

--- public/assets/js/locate_script.js
+++ public/assets/js/locate_script.js
@@ -49,4 +49,23 @@ function getCity(coordinates) {
 getCoordintes();
  */
 
-navigator.geolocation.getCurrentPosition(success[, error[, [options]])
+var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+  
+  function success(pos) {
+    var crd = pos.coords;
+  
+    console.log('Votre position actuelle est :');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude : ${crd.longitude}`);
+    console.log(`La précision est de ${crd.accuracy} mètres.`);
+  }
+  
+  function error(err) {
+    console.warn(`ERREUR (${err.code}): ${err.message}`);
+  }
+  
+  navigator.geolocation.getCurrentPosition(success, error, options);
