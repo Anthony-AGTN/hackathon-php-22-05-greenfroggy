b24f13eab120b7ec021dc3dce61e200cde650aa1
Cid-Jie <pcidjie@gmail.com>
2022-05-12 14:29:28 +0200

    add http request to call api weather

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -6,102 +6,22 @@ use App\Model\WeatherManager;
 
 class WeatherController extends AbstractController
 {
-
-
     /**
-     * List items
+     * List location
      */
     public function index(): string
     {
         $weatherManager = new WeatherManager();
-        $items = $itemManager->selectAll('title');
+        $location = $weatherManager->getLocationByName('Chambery');
 
-        return $this->twig->render('Item/index.html.twig', ['items' => $items]);
-    }
+        $insee = $location['cities'][0]['insee'];
 
-    /**
-     * List items
-     */
-    public function index(): string
-    {
-        $itemManager = new ItemManager();
-        $items = $itemManager->selectAll('title');
+        $weathers = $weatherManager->getWeatherByInsee($insee);
 
-        return $this->twig->render('Item/index.html.twig', ['items' => $items]);
-    }
-
-    /**
-     * Show informations for a specific item
-     */
-    public function show(int $id): string
-    {
-        $itemManager = new ItemManager();
-        $item = $itemManager->selectOneById($id);
-
-        return $this->twig->render('Item/show.html.twig', ['item' => $item]);
-    }
-
-    /**
-     * Edit a specific item
-     */
-    public function edit(int $id): ?string
-    {
-        $itemManager = new ItemManager();
-        $item = $itemManager->selectOneById($id);
-
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            // clean $_POST data
-            $item = array_map('trim', $_POST);
-
-            // TODO validations (length, format...)
-
-            // if validation is ok, update and redirection
-            $itemManager->update($item);
-
-            header('Location: /items/show?id=' . $id);
-
-            // we are redirecting so we don't want any content rendered
-            return null;
-        }
-
-        return $this->twig->render('Item/edit.html.twig', [
-            'item' => $item,
+        return $this->twig->render('Item/index.html.twig', [
+            'location' => $location,
+            'weathers' => $weathers
         ]);
     }
-
-    /**
-     * Add a new item
-     */
-    public function add(): ?string
-    {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            // clean $_POST data
-            $item = array_map('trim', $_POST);
-
-            // TODO validations (length, format...)
-
-            // if validation is ok, insert and redirection
-            $itemManager = new ItemManager();
-            $id = $itemManager->insert($item);
-
-            header('Location:/items/show?id=' . $id);
-            return null;
-        }
-
-        return $this->twig->render('Item/add.html.twig');
-    }
-
-    /**
-     * Delete a specific item
-     */
-    public function delete(): void
-    {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $id = trim($_POST['id']);
-            $itemManager = new ItemManager();
-            $itemManager->delete((int)$id);
-
-            header('Location:/items');
-        }
-    }
 }
+
