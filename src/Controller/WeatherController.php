ff95dc75800d81694dd74ff97f554bab6146bcbc
ghartemann <g.hartemann@gmail.com>
2022-05-12 17:15:42 +0200

    push sur dev

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -51,11 +51,66 @@ class WeatherController extends AbstractController
 
         $tempMin = $weathers['forecast'][0]['tmin'];
         $tempMax = $weathers['forecast'][0]['tmax'];
-        $optimist = $weatherService->optimistTemp ($tempMin, $tempMax);
+        $optimist = $weatherService->optimistTemp($tempMin, $tempMax);
         $realist = $weatherService->realistTemp($tempMin, $tempMax);
         $pessimist = $weatherService->pessimistTemp($tempMin, $tempMax);
 
         return $this->twig->render('Home/index.html.twig', [
+            'location' => $location,
+            'weathers' => $weathers,
+            'weatherPic' => $weatherPic,
+            'optimist' => $optimist,
+            'realist' => $realist,
+            'pessimist' => $pessimist
+        ]);
+    }
+
+    public function future(): string
+    {
+        if (($_SERVER['REQUEST_METHOD']) === 'POST') {
+            $errors = [];
+            $city = trim($_POST['city']);
+            if (empty($city)) {
+                $errors = 'error';
+            }
+            if (strlen($city) > 80) {
+                $errors = 'error';
+            }
+            if (empty($errors)) {
+                $weatherManager = new WeatherManager();
+                $location = $weatherManager->getLocationByName($city);
+                $insee = $location['cities'][0]['insee'];
+                $weathers = $weatherManager->getWeatherByInsee($insee);
+
+                $weather = $weathers['forecast'][0]['weather'];
+                $weatherService = new WeatherService();
+                $weatherPic = $weatherService->convertWeatherinPicture($weather);
+
+                return $this->twig->render('Home/index.html.twig', [
+                    'location' => $location,
+                    'weathers' => $weathers,
+                    'weatherPic' => $weatherPic
+                ]);
+            }
+        }
+
+        $weatherManager = new WeatherManager();
+        $location = $weatherManager->getLocationByName('lyon');
+        $insee = $location['cities'][0]['insee'];
+
+        $weathers = $weatherManager->getWeatherByInsee($insee);
+
+        $weather = $weathers['forecast'][0]['weather'];
+        $weatherService = new WeatherService();
+        $weatherPic = $weatherService->convertWeatherinPicture($weather);
+
+        $tempMin = $weathers['forecast'][0]['tmin'];
+        $tempMax = $weathers['forecast'][0]['tmax'];
+        $optimist = $weatherService->optimistTemp($tempMin, $tempMax);
+        $realist = $weatherService->realistTemp($tempMin, $tempMax);
+        $pessimist = $weatherService->pessimistTemp($tempMin, $tempMax);
+
+        return $this->twig->render('Future/index.html.twig', [
             'location' => $location,
             'weathers' => $weathers,
             'weatherPic' => $weatherPic,
--- src/routes.php
+++ src/routes.php
@@ -8,10 +8,5 @@
 // e.g route '/item/edit?id=1' will execute $itemController->edit(1)
 return [
     '' => ['WeatherController', 'index',],
-    'items' => ['ItemController', 'index',],
-    'items/edit' => ['ItemController', 'edit', ['id']],
-    'items/show' => ['ItemController', 'show', ['id']],
-    'items/add' => ['ItemController', 'add',],
-    'items/delete' => ['ItemController', 'delete',],
-    'weathers' => ['WeatherController', 'index',],
+    'future' => ['WeatherController', 'future'],
 ];
