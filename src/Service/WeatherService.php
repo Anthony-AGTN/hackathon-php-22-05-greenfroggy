81a89a791b5453c5c461fe60887a422cee4fc813
EmmanuelleAllain <e.allain13@gmail.com>
2022-05-12 15:45:02 +0200

    add picture weather

--- src/Service/WeatherService.php
+++ src/Service/WeatherService.php
@@ -2,27 +2,29 @@
 
 namespace App\Service;
 
+// this class transform the weather code (from API) in picture
 class WeatherService 
 {
-    public function convertWeather(int $weather) : int
+    public function convertWeatherinPicture(int $weather) : string
     {
         if ($weather == 0) {
-            $weatherIcon = //soleil
+            $weatherIcon = '/assets/images/Icons/sun.png';
         }
         if ($weather  > 0 && $weather < 7) {
-            $weatherIcon = // nuage
+            $weatherIcon = '/assets/images/Icons/cloudy.png';
         }
         if (($weather >= 10 && $weather < 20) 
         || ($weather >=40 && $weather <60)
         || ($weather >= 210)) {
-            $weatherIcon = // pluie
+            $weatherIcon = '/assets/images/Icons/cloudyrain.png';
         }
         if ($weather >= 20 && $weather <32
         || ($weather >=61 && $weather <79)) {
-            $weatherIcon = // neige
+            $weatherIcon = '/assets/images/Icons/snowy.png';
         }
         if ($weather >= 100 && $weather <143) {
-            $weatherIcon = // orage
+            $weatherIcon = '/assets/images/Icons/storm.png';
         }
+        return $weatherIcon;
     }
 }
