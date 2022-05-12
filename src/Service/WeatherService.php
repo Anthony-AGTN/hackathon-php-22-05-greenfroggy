6d16df68fa3aba4656850729ea9afc234fbc5da3
EmmanuelleAllain <e.allain13@gmail.com>
2022-05-12 15:10:47 +0200

    add weathericon function in service

--- src/Service/WeatherService.php
+++ src/Service/WeatherService.php
@@ -4,5 +4,25 @@ namespace App\Service;
 
 class WeatherService 
 {
-     return;
+    public function convertWeather(int $weather) : int
+    {
+        if ($weather == 0) {
+            $weatherIcon = //soleil
+        }
+        if ($weather  > 0 && $weather < 7) {
+            $weatherIcon = // nuage
+        }
+        if (($weather >= 10 && $weather < 20) 
+        || ($weather >=40 && $weather <60)
+        || ($weather >= 210)) {
+            $weatherIcon = // pluie
+        }
+        if ($weather >= 20 && $weather <32
+        || ($weather >=61 && $weather <79)) {
+            $weatherIcon = // neige
+        }
+        if ($weather >= 100 && $weather <143) {
+            $weatherIcon = // orage
+        }
+    }
 }
