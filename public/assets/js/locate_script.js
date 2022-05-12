0bab4f6a7033d3683777c29b22a6b36eb985711d
Anthony GOUTON <anthony.gouton.wcs@gmail.com>
2022-05-13 01:37:05 +0200

    ho bordel ça marche git add .!

--- public/assets/js/locate_script.js
+++ public/assets/js/locate_script.js
@@ -1,4 +1,4 @@
-/* // Step 1: Get user coordinates
+// Step 1: Get user coordinates
 function getCoordintes() {
     const options = {
         enableHighAccuracy: true,
@@ -39,33 +39,15 @@ function getCity(coordinates) {
     function processRequest(e) {
         if (xhr.readyState == 4 && xhr.status == 200) {
             const response = JSON.parse(xhr.responseText);
-            const city = response.address.city;
+            const city = response.address.municipality;
             console.log(city);
-            return;
+            document.getElementById("searchbar").value = city;
         }
     }
 }
 
-getCoordintes();
- */
+const locateButton = document.getElementById("locateButton");
 
-var options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
-  };
-  
-  function success(pos) {
-    var crd = pos.coords;
-  
-    console.log('Votre position actuelle est :');
-    console.log(`Latitude : ${crd.latitude}`);
-    console.log(`Longitude : ${crd.longitude}`);
-    console.log(`La précision est de ${crd.accuracy} mètres.`);
-  }
-  
-  function error(err) {
-    console.warn(`ERREUR (${err.code}): ${err.message}`);
-  }
-  
-  navigator.geolocation.getCurrentPosition(success, error, options);
+locateButton.addEventListener('click', function () {
+    getCoordintes()
+})
